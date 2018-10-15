883a92313785a6d73c8496372789b2843c42da5a
Reiko Watanabe <reiko.watanabe@hm.com>
2018-10-15 21:14:30 +0200

    improve logic

--- WebApplication2/WebApplication2/Controllers/BookController.cs
+++ WebApplication2/WebApplication2/Controllers/BookController.cs
@@ -4,18 +4,17 @@ using System.Linq;
 using System.Net.Http;
 using System.Web.Mvc;
 using WebApplication2.Models;
+using WebGrease.Css.Extensions;
 
 namespace WebApplication2.Controllers
 {
 
     public class BookController : Controller
     {
-       
-
         // GET: Book
-        public ActionResult Index(string bookGenre, string searchString, string author, string bookPrice) // ,string publishdata, string description
+        public ActionResult Index(string bookGenre, string searchString, string author, string bookPrice) // string description, string publishdate
         {
-            IEnumerable<BookViewModel> books = null;
+            IList<BookViewModel> books = null;
 
             using (var client = new HttpClient())
             {
@@ -31,39 +30,48 @@ namespace WebApplication2.Controllers
                     readTask.Wait();
 
                     books = readTask.Result;
-                    var bookViewModels = books.ToList();
 
-                    CreateDropdownData(bookViewModels);
+                    CreateDropdownData(books);
 
-                    if (!string.IsNullOrEmpty(searchString))
+                     if (!string.IsNullOrEmpty(searchString))
                     {
                         //if books are db objects, Contains runs on the database, not C#. on DB, Contains maps to SQL LIKE, which is case insensitive
                         //https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/adding-search
-                        books = bookViewModels.Where(b => b.Title.Contains(searchString));
+
+                        var idTitlestrings = new Dictionary<string, string[]>();
+                        books.ForEach(b => idTitlestrings.Add(b.Id, b.Title.ToLower().Split(' ')));
+                        var matchedTitles = new List<string>();
+                        foreach (var bookTitle in idTitlestrings)
+                        {
+                            if(bookTitle.Value.Any(w => w.Contains(searchString.ToLower())))
+                                matchedTitles.Add(bookTitle.Key);
+                        }
+
+                        books = books.Where(b => matchedTitles.Contains(b.Id)).ToList();
+
                     }
                     if (!string.IsNullOrEmpty(bookGenre))
                     {
-                        books = bookViewModels.Where(b => b.Genre.Equals(bookGenre));
+                        books = books.Where(b => b.Genre.Equals(bookGenre)).ToList();
                     }
 
                     if (!string.IsNullOrEmpty(author))
                     {
-                        books = bookViewModels.Where(b => b.Author.Equals(author));
+                        books = books.Where(b => b.Author.Equals(author)).ToList();
                     }
 
                     if (!string.IsNullOrEmpty(bookPrice))
                     {
-                        //Change logic to check price range 
-                        string[] words = bookPrice.Split(' ');
-                        var fromPrice = Convert.ToDecimal(words[0]);
-                        var toPrice = Convert.ToDecimal(words[2]);
-                        books = bookViewModels.Where(b => b.Price > fromPrice && b.Price < toPrice);
-
+                        var words = bookPrice.Split(' ');
+                        char[] toTrim = {'$'};
+                        var fromPrice = Convert.ToDecimal(words[0].Trim(toTrim));
+                        var toPrice = words.Length == 2 ? -99m :  Convert.ToDecimal(words[2].Trim(toTrim));
+                        books = toPrice < 0 ? books.Where(b => b.Price > fromPrice).ToList() : books.Where(b => b.Price > fromPrice && b.Price < toPrice).ToList();
                     }
                 }
                 else
                 {
-                    books = Enumerable.Empty<BookViewModel>();
+                    books = Enumerable.Empty<BookViewModel>().ToList();
                     ModelState.AddModelError(string.Empty, "Web api error.");
                 }
             }
@@ -80,17 +88,18 @@ namespace WebApplication2.Controllers
 
             //Price
             var priceCategories = new List<string>();
-            var priceRangeDictionary = new Dictionary<decimal, decimal>();
-            priceRangeDictionary.Add(1, 10);
-            priceRangeDictionary.Add(11, 20);
-            priceRangeDictionary.Add(21, 30);
-            priceRangeDictionary.Add(31, 100);
+            var priceRangeDictionary = new Dictionary<string, string>();
+            priceRangeDictionary.Add("1", "10");
+            priceRangeDictionary.Add("11", "20");
+            priceRangeDictionary.Add("21", "30");
+            priceRangeDictionary.Add("31", "");
 
             foreach (var range in priceRangeDictionary)
             {
-                priceCategories.Add($"{range.Key} - {range.Value}");
+                priceCategories.Add(string.IsNullOrEmpty(range.Value)
+                    ? $"${range.Key} - "
+                    : $"${range.Key} - ${range.Value}");
             }
-
             ViewBag.bookPrice = new SelectList(priceCategories);
 
         }
